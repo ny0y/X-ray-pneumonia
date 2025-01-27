0e195bf670b8bda7796cc900ab14228019dea2a7
Ahmed Almalki <153298420+ny0y@users.noreply.github.com>
2025-01-27 17:33:42 +0300

    Add files via upload

--- gpu.py
+++ gpu.py
@@ -2,3 +2,4 @@ import torch
 print(torch.cuda.is_available())
 print("CUDA Available: ", torch.cuda.is_available())
 print(torch.cuda.get_device_name(0))  # Prints the name of the GPU
+print(torch.cuda.get_device_name(0))  # Prints the name of the GPU
